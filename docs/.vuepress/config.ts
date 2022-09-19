99b9379dbc90e9f6eff291d96cfb0fcfdd00dd98
ycl <693812981@qq.com>
2022-09-19 19:31:31 +0800

    更新头部信息
    更新头部信息

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,7 +9,21 @@ import {
 export default defineUserConfig({
   base: "/vuepress-blog/",
   title: 'CL鱼塘',
-  head,
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        href: `/vuepress-blog/images/favicon.ico`,
+      },
+    ],
+    [
+      'link',
+      { rel: 'apple-touch-icon', href: `/vuepress-blog/images/logo.png` },
+    ],
+    ['meta', { name: 'msapplication-TileColor', content: '#3eaf7c' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+  ],
   theme: defaultTheme({
     logo: "/images/logo.png",
     home: "/",
