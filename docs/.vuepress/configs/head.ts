b1255bef0459e6a6e81d854ea2e713e7fc99ff69
CL <693812981@qq.com>
2022-10-16 16:46:27 +0800

    Update head.ts

--- docs/.vuepress/configs/head.ts
+++ docs/.vuepress/configs/head.ts
@@ -5,12 +5,12 @@ export const head: HeadConfig[] = [
     'link',
     {
       rel: 'icon',
-      href: `/images/favicon.ico`,
+      href: `/vuepress-blog/images/favicon.ico`,
     },
   ],
   [
     'link',
-    { rel: 'apple-touch-icon', href: `/images/logo.png` },
+    { rel: 'apple-touch-icon', href: `/vuepress-blog/images/logo.png` },
   ],
   ['meta', { name: 'msapplication-TileColor', content: '#3eaf7c' }],
   ['meta', { name: 'theme-color', content: '#3eaf7c' }],
